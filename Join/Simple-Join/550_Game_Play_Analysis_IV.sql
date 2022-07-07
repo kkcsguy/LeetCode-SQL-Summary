b7612d043f31ad869b4e4ebf4dc61eea7f77c753
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-06 22:58:45 -0700

    Update 550_Game_Play_Analysis_IV.sql

--- Join/Simple-Join/550_Game_Play_Analysis_IV.sql
+++ Join/Simple-Join/550_Game_Play_Analysis_IV.sql
@@ -3,11 +3,11 @@ SELECT ROUND(
     COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id),2) AS fraction
 FROM (
     -- get the first-logged-in date of each player
-    SELECT player_id, MIN(event_date) AS min_date
+    SELECT player_id, MIN(event_date) AS first_login
     FROM Activity
     GROUP BY player_id
 ) a1
 -- if a player logged back in on the day right after the first-logged-in date, 
 -- he/she would get a matched record from table a2
 LEFT JOIN Activity a2
-ON a1.player_id = a2.player_id AND DATE_ADD(a1.min_date,INTERVAL 1 DAY) = a2.event_date;
+ON a1.player_id = a2.player_id AND DATE_ADD(a1.first_login,INTERVAL 1 DAY) = a2.event_date;
