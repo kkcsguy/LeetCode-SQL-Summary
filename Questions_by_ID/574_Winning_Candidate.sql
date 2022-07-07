486bf498da3960d267652eee809cabd7a516cadb
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-07 02:36:18 -0700

    Update 574_Winning_Candidate.sql

--- Questions_by_ID/574_Winning_Candidate.sql
+++ Questions_by_ID/574_Winning_Candidate.sql
@@ -2,21 +2,23 @@
 SELECT Name
 FROM Candidate
 WHERE id = (
-    SELECT TOP 1 CandidateId
+    SELECT CandidateId
     FROM Vote
     GROUP BY CandidateId
     ORDER BY COUNT(*) DESC
+    LIMIT 1
 );
 
 
 
 -- Solution 2: Join
-SELECT TOP 1 c.Name
+SELECT c.Name
 FROM Candidate c
 JOIN Vote v
 ON c.id = v.CandidateId
 GROUP BY c.id, c.Name
-ORDER BY COUNT(*) DESC;
+ORDER BY COUNT(v.id) DESC
+LIMIT 1
 
 
 
