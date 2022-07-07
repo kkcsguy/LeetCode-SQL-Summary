4773b07a51a70599e46d8d7c609227a9fa4c95ec
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-07 02:20:40 -0700

    Update 550_Game_Play_Analysis_IV.sql

--- Subquery/Advanced-Subquery/550_Game_Play_Analysis_IV.sql
+++ Subquery/Advanced-Subquery/550_Game_Play_Analysis_IV.sql
@@ -1,19 +1,17 @@
 -- Solution 1: Subquery, Join
-SELECT CAST(
+SELECT ROUND(
     -- using DISTINCT to avoid double counting
-    COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id)
-    AS DECIMAL(3,2)
-) AS fraction
+    COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id),2) AS fraction
 FROM (
     -- get the first-logged-in date of each player
-    SELECT player_id, MIN(event_date) AS event_date
+    SELECT player_id, MIN(event_date) AS first_login
     FROM Activity
     GROUP BY player_id
 ) a1
 -- if a player logged back in on the day right after the first-logged-in date, 
 -- he/she would get a matched record from table a2
 LEFT JOIN Activity a2
-ON a1.player_id = a2.player_id AND DATEADD(day,1,a1.event_date) = a2.event_date;
+ON a1.player_id = a2.player_id AND DATE_ADD(a1.first_login,INTERVAL 1 DAY) = a2.event_date;
     
  
  
@@ -32,8 +30,6 @@ WITH tb2 as (
     WHERE dateadd(day,1, event_date) = lead_date
 )
 
-SELECT CAST(
-    COUNT(DISTINCT player_id)*1.0/(SELECT COUNT(DISTINCT player_id) FROM activity)
-    AS DECIMAL(3,2)
-    ) AS fraction
+SELECT ROUND(
+    COUNT(DISTINCT player_id)*1.0/(SELECT COUNT(DISTINCT player_id) FROM activity),2) AS fraction
 FROM tb2;
