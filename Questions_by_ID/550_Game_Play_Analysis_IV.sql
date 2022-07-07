e28a60d7f264e45cc166bf05cf1372bc7c4fe70b
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-07 02:26:19 -0700

    Update 550_Game_Play_Analysis_IV.sql

--- Questions_by_ID/550_Game_Play_Analysis_IV.sql
+++ Questions_by_ID/550_Game_Play_Analysis_IV.sql
@@ -1,19 +1,17 @@
 -- Solution 1: Subquery, Join
-SELECT CAST(
+SELECT ROUND(
     -- using DISTINCT to avoid double counting
-    COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id)
-    AS DECIMAL(3,2)
-) AS fraction
+    COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id),2) AS fraction
 FROM (
     -- get the first-logged-in date of each player
-    SELECT player_id, MIN(event_date) AS event_date
+    SELECT player_id, MIN(event_date) AS first_login
     FROM Activity
     GROUP BY player_id
 ) a1
 -- if a player logged back in on the day right after the first-logged-in date, 
 -- he/she would get a matched record from table a2
 LEFT JOIN Activity a2
-ON a1.player_id = a2.player_id AND DATEADD(day,1,a1.event_date) = a2.event_date;
+ON a1.player_id = a2.player_id AND DATE_ADD(a1.first_login,INTERVAL 1 DAY) = a2.event_date;
     
  
  
@@ -23,17 +21,15 @@ WITH tb2 as (
     FROM (
         SELECT player_id, 
             -- the first-logged-in date
-            MIN(event_date) OVER (PARTITION BY player_id) AS event_date,
+            MIN(event_date) OVER (PARTITION BY player_id) AS first_login,
             -- the next-logged-in date after first-logged-in date
-            LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS lead_date
+            LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS next_login
         FROM activity
         ) tb1
     -- only keep users the next-logged-in date after first-logged-in date is the day after first-logged in date
-    WHERE dateadd(day,1, event_date) = lead_date
+    WHERE DATE_ADD(first_login,INTERVAL 1 DAY) = next_login
 )
 
-SELECT CAST(
-    COUNT(DISTINCT player_id)*1.0/(SELECT COUNT(DISTINCT player_id) FROM activity)
-    AS DECIMAL(3,2)
-    ) AS fraction
+SELECT ROUND(
+    COUNT(DISTINCT player_id)*1.0/(SELECT COUNT(DISTINCT player_id) FROM activity),2) AS fraction
 FROM tb2;
