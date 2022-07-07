b7efd60801b7de320c089670fd10147ef0ee2a9e
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-07 14:00:51 -0700

    Update 1075_Project_Employees_I.sql

--- Join/Simple-Join/1075_Project_Employees_I.sql
+++ Join/Simple-Join/1075_Project_Employees_I.sql
@@ -1,4 +1,4 @@
-SELECT p.project_id, CAST(AVG(e.experience_years*1.0) AS DECIMAL(10,2)) AS average_years
+SELECT p.project_id, ROUND(AVG(e.experience_years*1.0),2) AS average_years
 FROM Project p
 LEFT JOIN Employee e
 ON p.employee_id = e.employee_id
