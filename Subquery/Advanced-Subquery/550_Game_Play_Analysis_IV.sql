70ff103efbb61ab30541a8c5fa20286ff154ec41
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-07 02:23:39 -0700

    Update 550_Game_Play_Analysis_IV.sql

--- Subquery/Advanced-Subquery/550_Game_Play_Analysis_IV.sql
+++ Subquery/Advanced-Subquery/550_Game_Play_Analysis_IV.sql
@@ -21,13 +21,13 @@ WITH tb2 as (
     FROM (
         SELECT player_id, 
             -- the first-logged-in date
-            MIN(event_date) OVER (PARTITION BY player_id) AS event_date,
+            MIN(event_date) OVER (PARTITION BY player_id) AS first_login,
             -- the next-logged-in date after first-logged-in date
-            LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS lead_date
+            LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS next_login
         FROM activity
         ) tb1
     -- only keep users the next-logged-in date after first-logged-in date is the day after first-logged in date
-    WHERE dateadd(day,1, event_date) = lead_date
+    WHERE DATE_ADD(first_login,INTERVAL 1 DAY) = next_login
 )
 
 SELECT ROUND(
