2aff5325c347daf2d9fc03cb6b01caf37c97b815
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-07 23:12:31 -0700

    Update 1270_All_People_Report_to_the_Given_Manager.sql

--- Join/Simple-Join/1270_All_People_Report_to_the_Given_Manager.sql
+++ Join/Simple-Join/1270_All_People_Report_to_the_Given_Manager.sql
@@ -1,16 +1,26 @@
--- find employees directly report to managers directly,
--- and then find people who report to managers indirectly using cte
-WITH cte AS (
-    SELECT employee_id
-    FROM Employees
-    WHERE manager_id = 1 AND employee_id != manager_id
-    UNION ALL
-    SELECT e.employee_id
+## Solution 1:
+WITH RECURSIVE cte AS (
+    SELECT 1 AS id, 1 AS t
+    UNION
+    SELECT e.employee_id AS id, t+1 AS t
     FROM Employees e
-    JOIN cte
-    ON e.manager_id = cte.employee_id
+    INNER JOIN cte c
+    ON e.manager_id = c.id
+    WHERE t < 4
 )
-
-SELECT employee_id
+SELECT DISTINCT id AS EMPLOYEE_ID
 FROM cte
-OPTION (MAXRECURSION 3);
+WHERE id != 1
+ORDER BY id
+
+
+## Solution 2:
+
+SELECT e1.employee_id 
+FROM   employees e1 
+       LEFT JOIN employees e2 
+              ON e1.manager_id = e2.employee_id 
+       LEFT JOIN employees e3 
+              ON e2.manager_id = e3.employee_id 
+WHERE  e3.manager_id = 1 
+       AND e1.employee_id != 1 
