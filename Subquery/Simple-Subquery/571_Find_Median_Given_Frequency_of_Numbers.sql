0d16ddec030bec2bbf9b82f718c19596e4c1d6ea
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-08 02:55:10 -0700

    Update 571_Find_Median_Given_Frequency_of_Numbers.sql

--- Subquery/Simple-Subquery/571_Find_Median_Given_Frequency_of_Numbers.sql
+++ Subquery/Simple-Subquery/571_Find_Median_Given_Frequency_of_Numbers.sql
@@ -3,11 +3,11 @@ WITH tb1 AS (
         -- There are cum_num numbers in TABLE numbers less than or equal to number in that record
         -- e.g. There are cum_num = 8 numbers in TABLE numbers less than or equal to 1
         -- so you will see [1,1,8,12] AS [Number, Frequency, cum_num, num]
-        SUM(frequency) OVER (ORDER BY number) AS cum_num,
-        SUM(frequency) OVER () AS num
+        SUM(frequency) OVER (ORDER BY num) AS cum_num,
+        SUM(frequency) OVER () AS num1
     FROM numbers
 )
 
-SELECT AVG(number*1.0) AS median
+SELECT AVG(num*1.0) AS median
 FROM tb1
-WHERE num / 2.0 BETWEEN cum_num - frequency AND cum_num;
+WHERE num1 / 2.0 BETWEEN cum_num - frequency AND cum_num;
