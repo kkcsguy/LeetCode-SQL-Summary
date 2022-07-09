03f9b327e58b91c98826fb093e3b39a174f6faf5
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-09 12:54:29 -0700

    Update 602_Friend_Requests_II_Who_Has_the_Most_Friends.sql

--- Subquery/Simple-Subquery/602_Friend_Requests_II_Who_Has_the_Most_Friends.sql
+++ Subquery/Simple-Subquery/602_Friend_Requests_II_Who_Has_the_Most_Friends.sql
@@ -1,9 +1,9 @@
 WITH tb1 AS (
     SELECT requester_id AS id
-    FROM request_accepted
+    FROM RequestAccepted
     UNION ALL
     SELECT accepter_id
-    FROM request_accepted
+    FROM RequestAccepted
 )
 
 SELECT TOP 1 id, COUNT(*) AS num
