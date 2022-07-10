4af639946704ef87ca60e692884dff8905823739
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-10 00:37:01 -0700

    Update 1113_Reported_Posts.sql

--- Questions_by_ID/1113_Reported_Posts.sql
+++ Questions_by_ID/1113_Reported_Posts.sql
@@ -1,11 +1,5 @@
--- You could update the variable to any date you want or GETDATE(), and then same analysis will be done for the new date 
-
--- Solution: Variable
-DECLARE @d date;
-SET @d = DATEADD(day,-1,'2019-07-05');
-
 SELECT extra AS report_reason, 
     COUNT(DISTINCT post_id) AS report_count
 FROM Actions
-WHERE Action_date = @d AND action = 'report'
+WHERE action_date = DATE_ADD('2019-07-05',INTERVAL -1 DAY) AND action = 'report'
 GROUP BY extra;
