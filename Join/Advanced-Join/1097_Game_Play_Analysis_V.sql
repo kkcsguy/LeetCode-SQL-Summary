638cd09986bf24c60b5572bc87b905a0225f4c8c
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-09 23:51:30 -0700

    Update 1097_Game_Play_Analysis_V.sql

--- Join/Advanced-Join/1097_Game_Play_Analysis_V.sql
+++ Join/Advanced-Join/1097_Game_Play_Analysis_V.sql
@@ -1,10 +1,9 @@
 SELECT a1.event_date AS install_dt,
     COUNT(DISTINCT a1.player_id) AS installs,
-    CAST(
+    ROUND(
         -- The number of players logged back in the day after / the number of players first logined on that date
         -- using DISTINCT to avoid double counting
-        COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id)
-        AS DECIMAL(3,2)
+        COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id),2
     ) AS Day1_retention
 FROM (
     -- get install date of each player
@@ -14,5 +13,5 @@ FROM (
 ) a1
 -- if a player logged back in on the day right after install date, he/she would get a matched record from table a2
 LEFT JOIN Activity a2
-ON a1.player_id = a2.player_id AND DATEADD(day,1,a1.event_date) = a2.event_date
+ON a1.player_id = a2.player_id AND DATEDIFF(a2.event_date,a1.event_date) = 1
 GROUP BY a1.event_date;
