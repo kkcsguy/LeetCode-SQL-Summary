486ce8ddc8f0377955a72af8a035849e1524b394
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-09 23:26:18 -0700

    Update 612_Shortest_Distance_in_a_Plane.sql

--- Join/Advanced-Join/612_Shortest_Distance_in_a_Plane.sql
+++ Join/Advanced-Join/612_Shortest_Distance_in_a_Plane.sql
@@ -1,11 +1,9 @@
-SELECT CAST(
+SELECT ROUND(
     MIN(
         SQRT(
             POWER(p1.x-p2.x,2)+POWER(p1.y-p2.y,2)
         )
-    )
-    AS DECIMAL(10,2)
-) AS shortest
-FROM point_2d p1
-JOIN point_2d p2
+    ),2) AS shortest
+FROM Point2D p1
+JOIN Point2D p2
 ON p1.x != p2.x OR p1.y != p2.y;
