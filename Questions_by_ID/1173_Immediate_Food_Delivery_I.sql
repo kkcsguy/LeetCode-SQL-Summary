a03a4a6a200b37f017264100517a748369fc33be
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-10 22:30:33 -0700

    Update 1173_Immediate_Food_Delivery_I.sql

--- Questions_by_ID/1173_Immediate_Food_Delivery_I.sql
+++ Questions_by_ID/1173_Immediate_Food_Delivery_I.sql
@@ -1,5 +1,5 @@
 -- Solution: CASE WHEN
-SELECT CAST(
+SELECT ROUND(
     AVG(
         -- using 100.0 rather than 100 is to ensure the type of number is float
         -- so that the number will keep digits after the decimal point when averaging
@@ -7,7 +7,5 @@ SELECT CAST(
             WHEN order_date = customer_pref_delivery_date THEN 100.0
             ELSE 0
         END
-       )
-    AS DECIMAL(5,2)
-) AS immediate_percentage
+       ),2) AS immediate_percentage
 FROM Delivery;
