eddd701c99a727696189fd9ed2f05fa991954b82
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-10 22:45:23 -0700

    Update 1211_Queries_Quality_and_Percentage.sql

--- Questions_by_ID/1211_Queries_Quality_and_Percentage.sql
+++ Questions_by_ID/1211_Queries_Quality_and_Percentage.sql
@@ -1,12 +1,9 @@
 -- Solution: CASE WHEN
 SELECT query_name, 
-    CAST(
-        AVG(rating*1.0/position) 
-        AS DECIMAL(10,2)
-    ) AS quality,
-    CAST(
-        AVG(CASE WHEN rating < 3 THEN 100.0 ELSE 0 END)
-        AS DECIMAL(10,2)
-    ) AS poor_query_percentage
+    ROUND(
+        AVG(rating*1.0/position),2) AS quality,
+    ROUND(
+        AVG(CASE WHEN rating < 3 THEN 100.0 ELSE 0 END),2) 
+        AS poor_query_percentage
 FROM Queries
 GROUP BY query_name;
