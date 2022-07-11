f214363bce8bb73372dab246f2a76c33c14f6e87
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-10 22:26:55 -0700

    Update 1142_User_Activity_for_the_Past_30_Days_II.sql

--- Questions_by_ID/1142_User_Activity_for_the_Past_30_Days_II.sql
+++ Questions_by_ID/1142_User_Activity_for_the_Past_30_Days_II.sql
@@ -6,7 +6,7 @@ SELECT CAST(
     AS DECIMAL(10,2)
     ) AS average_sessions_per_user
 FROM Activity
-WHERE activity_date BETWEEN DATEADD(day,-29,'2019-07-27') and '2019-07-27';
+WHERE activity_date BETWEEN DATE_ADD('2019-07-27',INTERVAL -29 DAY) and '2019-07-27';
 
 
 
@@ -14,12 +14,10 @@ WHERE activity_date BETWEEN DATEADD(day,-29,'2019-07-27') and '2019-07-27';
 WITH tb1 AS(
     SELECT COUNT(DISTINCT session_id) AS num
     FROM activity
-    WHERE activity_date BETWEEN DATEADD(day,-29,'2019-07-27') and '2019-07-27'
+    WHERE activity_date BETWEEN DATE_ADD('2019-07-27', INTERVAL -29 DAY) and '2019-07-27'
     GROUP BY user_id
 )
 
-SELECT CAST(
-    ISNULL(AVG(num*1.0),0)
-    AS DECIMAL(10,2)
-) AS average_sessions_per_user
+SELECT ROUND(
+    IFNULL(AVG(num*1.0),0),2) AS average_sessions_per_user
 FROM tb1;
