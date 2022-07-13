aa21f7a4b1498156b4795e67b1abaee8b5f82a6c
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-13 01:24:27 -0700

    Update 1084_Sales_Analysis_III.sql

--- Questions_by_ID/1084_Sales_Analysis_III.sql
+++ Questions_by_ID/1084_Sales_Analysis_III.sql
@@ -1,4 +1,4 @@
--- Solution: Subquery
+-- Solution 1: Subquery
 select product_id, product_name
 from product
 where product_id in (
@@ -11,3 +11,11 @@ where product_id in (
         where sale_date not between "2019-01-01" and "2019-03-31"
     )
 ) 
+
+
+-- Solution 2: Join
+select product_id, product_name
+from Sales join Product
+using(product_id)
+group by product_id
+having sum(sale_date between "2019-01-01" and "2019-03-31") = count(sale_date)
