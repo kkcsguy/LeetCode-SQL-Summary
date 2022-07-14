5763cf63287465e29dca37b540df029f7697dd6c
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-13 17:56:55 -0700

    Update 1285_Find_the_Start_and_End_Number_of_Continuous_Ranges.sql

--- Questions_by_ID/1285_Find_the_Start_and_End_Number_of_Continuous_Ranges.sql
+++ Questions_by_ID/1285_Find_the_Start_and_End_Number_of_Continuous_Ranges.sql
@@ -1,4 +1,4 @@
--- Solution 1: Window Function, Subquery
+-- Solution : Window Function, Subquery
 -- when the number is continous with the last one, both log_id and r add 1
 -- Therefore, continuous numbers share the same log_id - r
 -- when grouped by log_id - r, the start number is the samllest number in group and the end is the largest
@@ -11,17 +11,3 @@ SELECT MIN(log_id) AS START_ID, MAX(log_id) AS END_ID
 FROM tb1
 GROUP BY log_id-r;
 
-
-
--- Solution 2: Join, Subquery
-WITH tb1 AS (
-    SELECT l1.log_id, COUNT(*) AS r
-    FROM Logs l1
-    JOIN Logs l2
-    ON l1.log_id >= l2.log_id
-    GROUP BY l1.log_id
-)
-
-SELECT MIN(log_id) AS START_ID, MAX(log_id) AS END_ID
-FROM tb1
-GROUP BY log_id-r;
