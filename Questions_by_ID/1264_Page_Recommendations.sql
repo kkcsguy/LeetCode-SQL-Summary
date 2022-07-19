b3a26d33214308d0ded3406c0572a6abf696ef2f
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-19 00:06:37 -0700

    Update 1264_Page_Recommendations.sql

--- Questions_by_ID/1264_Page_Recommendations.sql
+++ Questions_by_ID/1264_Page_Recommendations.sql
@@ -1,14 +1,14 @@
--- Solution: CASE WHEN, Subquery
-WITH f AS (
-    SELECT CASE WHEN user1_id = 1 THEN user2_id ELSE user1_id END AS fid
-    FROM Friendship
-    WHERE user1_id = 1 OR user2_id =1 
-)
-
-SELECT DISTINCT page_id AS recommended_page
-FROM Likes
-WHERE user_id IN (SELECT * FROM f)
-EXCEPT
-SELECT page_id
-FROM Likes
-WHERE user_id = 1;
+-- Solution: Subquery
+SELECT DISTINCT page_id AS recommended_page 
+FROM   likes l2 
+WHERE  NOT EXISTS (SELECT *
+                   FROM   likes l1 
+                   WHERE  l1.user_id = 1 
+                          AND l1.page_id = l2.page_id) 
+       AND user_id IN (SELECT user2_id AS friend_id 
+                       FROM   friendship 
+                       WHERE  user1_id = 1 
+                       UNION 
+                       SELECT user1_id AS friend_id 
+                       FROM   friendship 
+                       WHERE  user2_id = 1) 
