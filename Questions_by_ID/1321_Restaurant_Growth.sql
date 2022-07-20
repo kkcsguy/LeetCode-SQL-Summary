e6c680db4864f3f8659594a155b9908dec7fef25
kkcsguy <78928736+kkcsguy@users.noreply.github.com>
2022-07-20 12:22:00 -0700

    Update 1321_Restaurant_Growth.sql

--- Questions_by_ID/1321_Restaurant_Growth.sql
+++ Questions_by_ID/1321_Restaurant_Growth.sql
@@ -1,20 +1,18 @@
--- Solution 1: Window Function, OFFSET FETCH
-SELECT visited_on, 
-    SUM(SUM(amount)) OVER (
-        ORDER BY visited_on 
-        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-    ) AS amount,
-    CAST(
-        SUM(SUM(amount)) OVER (
-            ORDER BY visited_on 
-            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-        )/7.0
-        AS  DECIMAL(10,2)
-    )  AS average_amount
-FROM Customer
-GROUP BY visited_on
-ORDER BY visited_on
-OFFSET 6 ROWS;
+-- Solution 1: Window Function
+WITH total_cte AS
+(
+  SELECT DISTINCT visited_on, 
+         SUM(amount) OVER (ORDER BY visited_on
+                           RANGE BETWEEN INTERVAL 6 DAY PRECEDING
+                           AND CURRENT ROW) AS amount
+  FROM Customer
+)
+
+SELECT visited_on, amount, ROUND(amount/7, 2) AS average_amount
+FROM total_cte AS a
+INNER JOIN
+(SELECT MIN(visited_on) AS min_visited_on FROM total_cte) AS b
+ON DATEDIFF(visited_on, min_visited_on) >= 6;
 
 
 
